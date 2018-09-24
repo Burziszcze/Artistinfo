29659595fe9dc453bf1911a4adde5deaacfaa595
burziszcze <buurzuum@gmail.com>
2018-09-24 15:15:44 +0200

    sad

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import lfmlogo from '../images/Lastfm_logo.svg';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="header-content container d-inline-flex">
+    <header className="header container">
+      <div className="header-content d-inline-flex">
         <img src={lfmlogo} alt="logo" className="logo" />
         <h2 className="hd-header ml-3">ArtistInfo</h2>
         {/* <p className="hd-title">this app uses data from lastfm api</p> */}
@@ -14,4 +14,3 @@ const Header = () => {
 }
 
 export default Header;
-``
