3c1151f782d35194607ad1017535fd3ad972fc96
burziszcze <buurzuum@gmail.com>
2018-09-25 13:58:38 +0200

    some dir structure gonna be changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import lfmlogo from '../images/Lastfm_logo.svg';
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-content container d-flex justify-content-start">
+      <div className="header-content d-flex flex-wrap justify-content-start">
         <img src={lfmlogo} alt="logo" className="logo" />
         <h2 className="hd-header ml-3">ArtistInfo</h2>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -40,7 +40,7 @@ class Main extends Component {
   fetchData = event => {
     let key = process.env.REACT_APP_API_KEY;
     let url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${event}&lang=pl&api_key=${key}&format=json`;
-
+    console.log(url);
     // API call
     axios
       .get(url)
@@ -80,35 +80,35 @@ class Main extends Component {
     const { name, bio, image, url, ontour, similar, listeners, playcount, tags } = this.state;
 
     return (
-      <main className="main">
-        <div className="container">
+      <main className="main container">
+        {/* <div className="container">
+          <div className="row"> */}
+        <div className="col-xs-12 col-lg-10 offset-lg-1 search-box">
+          <SearchBox
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+            onSubmit={this.handleSubmit}
+          />
+        </div>
+        <div className="col-xs-12 col-lg-10 offset-lg-1 artist-card">
           <div className="row">
-            <div className="search-box col-xs-12 col-lg-10 offset-lg-1">
-              <SearchBox
-                value={this.state.inputValue}
-                onChange={this.handleChange}
-                onSubmit={this.handleSubmit}
+            <div className="col-xs-12 col-md-4 pull-md-8 col-lg-5 pull-lg-7 text-center artist-img">
+              <img src={image} alt={name}
+                className="img-fluid"
               />
             </div>
-            <div className="artist-card col-xs-12 col-lg-10 offset-lg-1">
-              <div className="row">
-                <div className="artist-img col-xs-12 col-md-4 pull-md-8 col-lg-5 pull-lg-7 text-center">
-                  <img src={image} alt={name}
-                    className="img-fluid"
-                  />
-                </div>
-                <div className="artist-content col-xs-12 col-md-8 push-md-4 col-lg-7 push-lg-5">
-                  <h1>{name}</h1>
-                  <h4>listeners: {listeners}</h4>
-                  <h3>playcount: {playcount}</h3>
-                  <h3>Biography:</h3>
-                  <p>{bio}</p>
-                </div>
-              </div>
+            <div className="col-xs-12 col-md-8 push-md-4 col-lg-7 push-lg-5 artist-content">
+              <h1>{name}</h1>
+              <h4>listeners: {listeners}</h4>
+              <h3>playcount: {playcount}</h3>
+              <h3>Biography:</h3>
+              <p>{bio}</p>
             </div>
           </div>
         </div>
-      </main>
+        {/* </div>
+        </div > */}
+      </main >
     )
   }
 }
